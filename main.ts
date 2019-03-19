e199b50ab6adbe9bc35a16912879971aad4500d6
mario <mariohenrique2520@hotmail.com>
2019-03-19 14:59:57 -0700

    commit

--- main.ts
+++ main.ts
@@ -1,3 +1,45 @@
+// Here are the different types of blocks
+// supported by MakeCode
+
+//% color="#4C97FF"
+namespace basic {
+
+    /**
+     * This is a statement block
+     */
+    //% block
+    export function setup() {
+        let countB = 0;
+        let countA = 0;
+        let teacher = 12345;
+        radio.setGroup(1);
+    }
+
+    /**
+     * This is a statement block
+     */
+    //% block
+    export function reset() {
+
+    }
+
+    /**
+     * This is a statement block
+     */
+    //% block
+    export function showResults() {
+
+    }
+
+    /**
+     * This is a statement block with a parameter
+     */
+    //% block
+    export function countVote(vote: number) {
+
+    }
+}
+
 basic.showLeds(`
     . . . . .
     . # . # .
